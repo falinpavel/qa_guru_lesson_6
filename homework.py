f561208dd22ebfab3c098d1a7fc764dd18733ce3
Falin Pavel <falinpavel96@gmail.com>
2025-07-24 17:54:27 +0300

    hw done

--- homework.py
+++ homework.py
@@ -68,6 +68,16 @@ def test_find_suitable_user():
 # сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
+def my_function(function, *args, **kwargs):
+    function_name = function.__name__.replace('_', ' ').title()
+    all_values = list()
+    all_values.extend(args) if args else all_values.extend(kwargs.values())
+    # if args:
+    #     all_values.extend(args)
+    # if kwargs:
+    #     all_values.extend(kwargs.values())
+    args_str = ", ".join(str(value) for value in all_values)
+    return f"{function_name} [{args_str}]"
 
 
 def test_readable_function():
@@ -77,15 +87,20 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    # вот тут вызываем реализованную функцию, которая вернет нам читабельное имя самой функции и аргументов
+    actual_result = my_function(open_browser, browser_name=browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = my_function(go_to_companyname_homepage, page_url=page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = my_function(
+        find_registration_button_on_login_page,
+        page_url=page_url,
+        button_text=button_text
+    )
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
