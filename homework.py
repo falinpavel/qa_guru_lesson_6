b476872f8ebd533a840084dcce0f63d90200487a
Falin Pavel <falinpavel96@gmail.com>
2025-07-25 13:39:18 +0300

    fix test_dark_theme_by_time_and_user_choice

--- homework.py
+++ homework.py
@@ -21,12 +21,16 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
     current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
+    dark_theme_enabled_by_user = None
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = (False if current_time >= time(hour=22) <= time(hour=6) else True) \
-        if dark_theme_enabled_by_user is None else dark_theme_enabled_by_user
+    # is_dark_theme = (False if current_time >= time(hour=22) <= time(hour=6) else True) \
+    #     if dark_theme_enabled_by_user is None else dark_theme_enabled_by_user
+    if dark_theme_enabled_by_user:
+        is_dark_theme = False if current_time >= time(hour=22) <= time(hour=6) else True
+    else:
+        is_dark_theme = dark_theme_enabled_by_user
     assert is_dark_theme is True
 
 
@@ -86,11 +90,13 @@ def test_readable_function():
 
 def open_browser(browser_name):
     actual_result = my_function(open_browser, browser_name=browser_name)
+    print(f"{actual_result}")
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = my_function(go_to_companyname_homepage, page_url=page_url)
+    print(f"{actual_result}")
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
@@ -100,4 +106,5 @@ def find_registration_button_on_login_page(page_url, button_text):
         page_url=page_url,
         button_text=button_text
     )
+    print(f"{actual_result}")
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
