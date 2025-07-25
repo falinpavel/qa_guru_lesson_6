7a38ce01bbdca8e6258f161dacc318224deaaf0b
Falin Pavel <falinpavel96@gmail.com>
2025-07-25 14:06:09 +0300

    fix test_dark_theme_by_time_and_user_choice

--- homework.py
+++ homework.py
@@ -82,6 +82,7 @@ def my_function(function, *args, **kwargs):
     all_values = list(kwargs.values() if kwargs else args)
     # all_values.extend(args) if args else all_values.extend(kwargs.values())
     args_str = ", ".join(str(value) for value in all_values)
+    print(f"{function_name} [{args_str}]")
     return f"{function_name} [{args_str}]"
 
 
@@ -93,13 +94,11 @@ def test_readable_function():
 
 def open_browser(browser_name):
     actual_result = my_function(open_browser, browser_name=browser_name)
-    print(f"{actual_result}")
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
     actual_result = my_function(go_to_companyname_homepage, page_url=page_url)
-    print(f"{actual_result}")
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
@@ -109,5 +108,4 @@ def find_registration_button_on_login_page(page_url, button_text):
         page_url=page_url,
         button_text=button_text
     )
-    print(f"{actual_result}")
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
